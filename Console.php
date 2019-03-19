068885e497a7255eb729aef5d0350cacc33025c3
df-solutions <lacertaz@yandex.ru>
2019-03-19 18:33:35 +0300

    Initial commit

---
+++ Console.php
@@ -0,0 +1,31 @@
+<?php 
+
+class Console
+{
+    private static $messages = [];
+
+    public static function output()
+    {
+        echo '<div class="notice">';
+        foreach (self::$messages as $key => $message) {
+            if (is_array($message) || is_object($message)) {
+                echo '<pre>' . print_r($message, true) . '</pre>';
+            } else if (is_null($message)) {
+                echo "<p>null</p>";
+            } else {
+                echo "<pre>$message</pre>";
+            }
+        }
+        echo '</div>';
+    }
+
+    public static function init()
+    {
+        add_action('admin_notices', [get_class(), 'output']);
+    }
+
+    public static function log($message = null)
+    {
+        self::$messages[] = $message;
+    }
+}
